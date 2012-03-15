8809037c60c0292f40b32c28508d504494df488b
Krzysiek Nowak <kiryx7@gmail.com>
2012-03-16 00:22:10 +0100

    hello

--- plik.py
+++ plik.py
@@ -1,4 +1,10 @@
+def costam():
+	hello hello#NOT WORKING
+
 def fun():
-	print "HELLO"
+	print "HELLO"#Doesn't work
+#what the fuck
+#IS THIS
 def hello():
 	print "Hello2"
+#SOME COMMENTS AT THE END
