58138cb216ca212b51f8b4a89e7e01abdcf51041
Ania Warpechowska <awarpechowska@gmail.com>
2012-03-18 23:42:47 +0100

    zrobione wszystko

--- SWD.py
+++ SWD.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 class Zbior:
 	def __init__(self):
 		self.lista = []
@@ -27,14 +29,17 @@ class Zbior:
 		
 		if (nazwy_kolumn):
 			self.kolumny = f[skip].split(separator)
-		else: 
+		else:
 			skip -=1
 		
 		for i in f[skip+1:]:
 			self.lista.append(i.split(separator))
 		for i in range(len(self.lista[0])):
 			self.typy.append(unicode)
-			
+		
+		if(not nazwy_kolumn):
+			for i in range(len(self.lista[0])):
+				self.kolumny.append('kolumna '+str(i))
 		return self.lista
 		
 	def usun_obserwacja(self, ktora):
@@ -49,7 +54,10 @@ class Zbior:
 	def rzutuj(self, na_co, ktory):
 		self.typy[ktory] = na_co
 		for i in range(len(self.lista)):
-			self.lista[i][ktory] = na_co(self.lista[i][ktory])
+			if(na_co == float):
+				self.lista[i][ktory] = na_co(self.lista[i][ktory].replace(',','.'))
+			else:
+				self.lista[i][ktory] = na_co(self.lista[i][ktory])
 	
 	def dyskretyzuj(self,ktora, n):
 		min = self.lista[0][ktora]
@@ -108,6 +116,61 @@ class Zbior:
 		return checked
 	
 	def standaryzuj(self, ktora):
+		suma = 0.00
+		for i in range(len(self.lista)):
+			suma += self.lista[i][ktora]
+
+		srednia = suma/ len(self.lista)
+		wariancja = 0.00
+		for i in range(len(self.lista)):
+			wariancja += (self.lista[i][ktora] - srednia)**2
+		wariancja = wariancja / len(self.lista)
+		odchylenie = sqrt(wariancja)
+		
+		for i in range(len(self.lista)):
+			self.lista[i].append(round(float((float(self.lista[i][ktora]-srednia))/float(odchylenie)),2))
+		
+		self.kolumny.append('Standaryzacja: ' + self.kolumny[ktora])
+		self.typy.append(float)
+		
+	def normalizuj(self, w_min, w_maks, ktora):
+		min = self.lista[0][ktora]
+		max = self.lista[0][ktora]
+		for i in range(len(self.lista)):
+			if(min > self.lista[i][ktora]):
+				min = self.lista[i][ktora]
+			if(max < self.lista[i][ktora]):
+				max = self.lista[i][ktora]
+
+		for i in range(len(self.lista)):
+			tmp = float((self.lista[i][ktora]-min)*(w_maks-w_min)/(max-min))
+			self.lista[i].append(round(tmp + w_min,2))
+		
+		self.kolumny.append('Normalizacja: ' + self.kolumny[ktora])
+		self.typy.append(float)
+	
+	def odchylenie_trzykrotne(self, ktora):
+		suma = 0.00
+		for i in range(len(self.lista)):
+			suma += self.lista[i][ktora]
+
+		srednia = suma/ len(self.lista)
+		wariancja = 0.00
+		for i in range(len(self.lista)):
+			wariancja += (self.lista[i][ktora] - srednia)**2
+		wariancja = wariancja / len(self.lista)
+		odchylenie = sqrt(wariancja)
+		
+		for i in range(len(self.lista)):
+			if (abs(self.lista[i][ktora]-srednia) > abs(3 * odchylenie)):
+				self.lista[i].append(1)
+			else:
+				self.lista[i].append(0)
+		
+		self.kolumny.append('Czy odstaje: ' + self.kolumny[ktora])
+		self.typy.append(bool)
+		
+	def odstajace_procentowo(self, proc_min, proc_maks, ktora):
 		min = self.lista[0][ktora]
 		max = self.lista[0][ktora]
 		for i in range(len(self.lista)):
@@ -116,21 +179,26 @@ class Zbior:
 			if(max < self.lista[i][ktora]):
 				max = self.lista[i][ktora]
 		for i in range(len(self.lista)):
-			self.lista[i].append(round(float((float(self.lista[i][ktora]-min))/float((max-min))),2))
-		
-		self.kolumny.append('Standaryzacja: ' + self.kolumny[ktora])
-		self.typy.append(float)
-		
-	#def normalizuj(self, w_min, w_maks):
-		
-
+			tmp = float(self.lista[i][ktora]-min)/ float(max-min)
+			if(tmp< proc_min or tmp > proc_maks):
+				self.lista[i].append(1)
+			else:
+				self.lista[i].append(0)
+				
+		self.kolumny.append('Czy odstaje proc: ' + self.kolumny[ktora])
+		self.typy.append(bool)
+			
 if(__name__ == "__main__"):
 	z = Zbior()
 	z.wczytaj('dane.txt', '    ', 11, 1)
 	z.rzutuj(int, 7)
 	z.rzutuj(str, 1)
+	z.rzutuj(float, 4)
 	#z.usun_zmienna(0)
 	#z.dyskretyzuj(7,3)
 	#z.dyskretyzuj2(7,3)
-	z.standaryzuj(7)
+	#z.standaryzuj(7)
+	#z.normalizuj(10,100,7)
+	z.odchylenie_trzykrotne(7)
+	z.odstajace_procentowo(0.1, 0.9, 4)
 	print z
